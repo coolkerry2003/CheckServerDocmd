600d6bb88a8d60403f3a00364509c61e6e4e3d72
祐祐 └ <coolkerry2003@hotmail.com.tw>
2020-04-15 13:48:52 +0800

    加入專案檔案。

---
+++ CheckServerDocmd/Program.cs
@@ -0,0 +1,45 @@
+﻿using PilotGaea.Serialize;
+using PilotGaea.TMPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckServerDocmd
+{
+    public class Program : DoCmdBaseClass
+    {
+        VarStruct varStruct = new VarStruct();
+
+        public Program()
+        {
+        }
+        public override bool Init()
+        {
+            return true;
+        }
+        public override void DeInit()
+        {
+        }
+        public override bool DoCmd(CGeoDatabase DB, string Cmd, string SessionID, VarStruct InputParm, out VarStruct RetParm)
+        {
+            RetParm = new VarStruct();
+            bool docmd = false;
+
+            switch (Cmd)
+            {
+                case "isLive":
+                    docmd = true;
+                    break;
+            }
+            return docmd;
+        }
+        public override string[] GetSupportCmds()
+        {
+            //Check DoCommand有哪些方法
+            string[] varGetAll = new string[] { "isLive" };//
+
+            return varGetAll;
+        }
+    }
+}
